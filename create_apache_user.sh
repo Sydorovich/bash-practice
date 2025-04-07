00bdc65a3c314c6b398677deca21c30b80be1a0b
Sydorovich <pavvadymq@gmail.com>
2025-04-07 22:49:42 +0200

    Add README and create_apache_user.sh script with initial documentation

---
+++ create_apache_user.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# This script creates a user for Apache with the specified username and password.
+# It also creates a home directory for the user and sets the appropriate permissions.
+
